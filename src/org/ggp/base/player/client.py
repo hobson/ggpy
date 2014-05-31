abbf9178df41712d6e9032521eb0691299ef6348
Hobson Lane <hobsonlane@gmail.com>
2014-05-31 16:27:32 -0700

    server.Server() fails to respond or connect to locally running game server test()

---
+++ src/org/ggp/base/player/client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()
+
+host = socket.gethostname() # Get local machine name
+port = 9147                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
--- src/org/ggp/base/player/server.py
+++ src/org/ggp/base/player/server.py
@@ -2,14 +2,40 @@
 
 import socket               # Import socket module
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
 
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   c.close()                # Close the connection
+class ServerSocket():
+    '''Consider using SocketServer instead: https://docs.python.org/2/library/socketserver.html'''
+    port0 = 9147
+    listeners = {-1: None}
+
+    def __init__(self, num=0):
+        # TODO: reuse closed socket IDs
+        if num is None:
+            self.num = max(ServerSocket.listeners) + 1
+        else:
+            self.num = num
+        self.listener = socket.socket()
+        self.host = socket.gethostname()  # Get local machine name
+        self.port = self.port0 + self.num  # Reserve a port for this game player
+
+        try:
+            self.listener.bind((self.host, self.port))  # Bind to the port
+            ServerSocket.listeners[self.num] = self.listener
+        except:  # can't find a more specific Exception for 
+            self.listener.close()
+            self.__init__(num=self.num + 1)
+
+    def shutdown(self):
+        self.listener.close()
+        del(ServerSocket.listeners[self.num])
+
+    def listen(self, backlog=5):
+        try: 
+            self.listener.listen(backlog)      # Now wait for client connection, allowing a backlog of up to 5 connections in the queue.
+            while True:
+               c, addr = self.listener.accept()     # Establish connection with client.
+               print 'Got connection from', addr
+               c.send('Thank you for connecting')
+               c.close()                # Close the connection
+        except KeyboardInterrupt:
+            self.shutdown()
