f260df2ae802d42b3ade699539db682025f4e2eb
Hobson Lane <hobsonlane@gmail.com>
2014-07-16 08:11:05 -0700

    outer parentheses optional in GDL

--- ggpy/gdl/parser.py
+++ ggpy/gdl/parser.py
@@ -1,31 +1,34 @@
 import pyparsing as pp
 
-# capital letter followed by any number of alphanum or underscore chars
+# function constants are usually lowercase, but haven't found that as a hard requirement in the spec
 function_constant = pp.Word(pp.srange("[A-Za-z]"), pp.srange("[a-zA-Z0-9_]"))
 identifier = pp.Word(pp.srange("[A-Za-z]"), pp.srange("[a-zA-Z0-9_]"))
 comment = pp.OneOrMore(pp.Word(';').suppress()) + pp.restOfLine('comment')
 
 # GDL keywords ("Relation Constants")
-role = pp.Keyword('role')  # role(a) means that a is a player name/side in the game.
+role = pp.Keyword('role')  # role(p) means that p is a player name/side in the game.
 inpt = pp.Keyword('input') # input(t) means that t is a base proposition in the game.
 base = pp.Keyword('base')  # base(a) means that a is an action in the game.
 init = pp.Keyword('init')  # init(p) means that the datum p is true in the initial state.
 next = pp.Keyword('next')  # next(p) means that the datum p is true in the next state.
-does = pp.Keyword('does')  # does(r,a) means that player r performs action a in the current state.
-legal = pp.Keyword('legal')  # legal(r,a) means it is legal for r to play a in the current state.
-goal = pp.Keyword('goal')  # goal(r,n) means that player the current state has utility n for player r. n must be an integer from 0 through 100.
-terminal = pp.Keyword('terminal')  # terminal means that the current state is a terminal state.
-distinct = pp.Keyword('distinct')  # distinct(x,y) means that the values of x and y are different.
+does = pp.Keyword('does')  # does(r, a) means that player r performs action a in the current state.
+legal = pp.Keyword('legal')  # legal(r, a) means it is legal for r to play a in the current state.
+goal = pp.Keyword('goal')  # goal(r, n) means that player the current state has utility n for player r. n must be an integer from 0 through 100.
+terminal = pp.Keyword('terminal')  # terminal(d) means that if the datam d is true, the game has ended and no player actions are legal.
+distinct = pp.Keyword('distinct')  # distinct(x, y) means that the values of x and y are different.
 true = pp.Keyword('true')  # true(p) means that the datum p is true in the current state.
 
-variable = pp.Word('?', pp.alphas)
+# GDL-I Relation Constants
+relation_constant = role | inpt | base | init | next | does | legal | goal | terminal | distinct | true
 
 # GDL-II Relation Constants
 sees = pp.Keyword('sees')  # The predicate sees(?r,?p) means that role ?r perceives ?p in the next game state.
 random = pp.Keyword('random')  # A predefined player that choses legal moves randomly
 
-# GDL-I Relation Constants
-relation_constant = role | inpt | base | init | next | does | legal | goal | terminal | distinct | true
+# other tokens/terms
+variable = pp.Word('?', pp.alphas)
+operator = pp.Word('~&|')  # not, and, or
+identifier = pp.Word(pp.alphas + '_', pp.alphas + pp.nums + '_')
 
 # Numerical contant
 # FIXME: too permissive -- accepts 10 numbers, "00", "01", ... "09"
@@ -38,8 +41,15 @@ token = (implies | variable | relation_constant | number | pp.Word(pp.alphas + p
 
 # Define recursive grammar for nested paretheticals
 grammar = pp.Forward()
+expression = pp.OneOrMore(implies | variable | relation_constant | number | operator | identifier)
 nested_parentheses = pp.nestedExpr('(', ')', content=grammar) 
-grammar << (implies | variable | relation_constant | number | pp.Word(pp.alphas + pp.nums) | nested_parentheses)
-sentence = grammar + (comment | pp.lineEnd.suppress())
+grammar << (implies | variable | relation_constant | number | operator | identifier | nested_parentheses)
+sentence = (expression | grammar) + (comment | pp.lineEnd.suppress() | pp.stringEnd.suppress())
 
 game_description = pp.OneOrMore(comment | sentence)
+
+def test():
+    import os
+    gdl_path = os.path.join(os.path.dirname(__file__), '..', 'example_game_gdl', 'chapter10.gdl')
+    parsed_gdl = game_description.parseFile(gdl_path)
+    return parsed_gdl
--- ggpy/gdl/propnet.py
+++ ggpy/gdl/propnet.py
@@ -11,11 +11,17 @@ import networkx as nx
 import operator
 
 import parser 
+import os
+
+from pyparsing import ParseResults
 
 class Node(object):
     def __init__(self, label=''):
         self.value = None
-        self.label = label
+        if isinstance(label, basestring):
+            self.label = label
+        else:
+            self.label = label[0]
     def __repr__(self):
         return "%s('%s')" % (self.__class__.__name__, self.label)
     __str__ = __repr__
@@ -72,10 +78,81 @@ class Terminal(Node):
         if self.value:
             raise StopIteration
 
+
 class Goal(Node):
     """A non-boolean valued node which indicates the rewuard for a player as an integer from 0 to 100"""
     def __init__(self, label=''):
         super(Terminal, self).__init__(label)
+
+
+def listify(list_or_obj):
+    if (hasattr(list_or_obj, 'append') and hasattr('__iter__')) or isinstance(list_or_obj, (list, ParseResults)):
+        return list_or_obj
+    return [list_or_obj]
+
+
+# keyword_meta =  { 
+#                 'role': {'nargs': 1},
+#                 'base': {'nargs': 1},
+#                 'input': {'nargs': 2},
+#                 'legal': {'nargs': 2},
+#                 }
+
+
+class LogicNetwork(nx.MultiDiGraph):
+
+    def __init__(self, *args, **kwargs):
+        return super(LogicNetwork, self).__init__(self, *args, **kwargs)
+
+    def add_node(self, type=None, name=None, mark=None, source=None):
+        print 'add_node'
+        if name is None:
+            name = self.number_of_nodes()
+        if type is None:
+            type = 'buffer'  # MIL-STD/ANSI/IEC term for "pass-through"/not-not/No-Op 
+        attr = { 'type': type, 'name': name, 'mark': mark, 'source': source }
+        print attr
+        super(LogicNetwork, self).add_node(name, attr=attr)
+
+    def parse_action(self, *args):
+        '''Action to perform (add_node) when a new GDL token (word) is successfully parsed.
+           A parse action must accept 0-3 arguments, and is called by pyparsing with
+           C{fn(s,loc,toks)}, C{fn(loc,toks)}, C{fn(toks)}, or just C{fn()}, where:
+            - s    = the original string parsed
+            - loc  = the location of the matching substring within the origial string
+            - toks = a list of the matched tokens, packaged as a C{L{ParseResults}} object
+        '''
+        if not args:
+            return
+        if len(args) == 3:
+            s, loc, toks = args
+        elif len(args) == 2:
+            loc, toks = args
+        elif len(args) == 1:
+            toks = args
+        print 'parse_action(s, loc, toks=%s)' % toks
+        toks = listify(toks)
+        print 'parse_action(s, loc, toks=%s)' % toks
+        print len(toks)
+        toks[0] = listify(toks[0])
+        print len(toks[0])
+        if len(toks[0]) > 1:
+            toks[0][1] = toks[0][1:]
+        name, type = None, toks[0][0]
+        if type in ['role', 'base']:
+            # print toks, toks[0][0], ' '.join(toks[0][1])
+            name = '-'.join(toks[0][1])
+            # print 'adding node named %s ...' % name
+        elif type in ['legal', 'input']:
+            # print toks, toks[0][0], ' '.join(toks[0][1])
+            name = 'does(' + ','.join(toks[1][1]) + ')'
+            # print 'adding node named %s ...' % name
+        elif type == '<=':
+            pass
+        print name
+        if name:
+            self.add_node(type=type, name=name, mark=None, source=None)
+        # print 'added node, now have %s' % self.nodes()
 
 
 class View(Node):
@@ -83,12 +160,43 @@ class View(Node):
     def __init__(self, label=''):
         super(View, self).__init__(label)
 
-def network_from_parsed_gdl(parsed_gdl):
-    raise NotImplementedError('Not yet implemented')
-    return parsed_gdl
+def propnet_from_parsed_gdl(parsed_gdl):
+    propnet = LogicNetwork()
+    #parser.sentence.setParseAction(propnet.parse_action)
+    for i, fact_tokens in enumerate(parsed_gdl):
+        if (fact_tokens[0] in ['base', 'role', 'input']):
+            name = ' '.join(fact_tokens[1])
+            propnet.add_node(name=name, type=fact_tokens[0])
+    return propnet
+
+def noop(toks):
+    print toks
+    return toks
+
+def propnet_from_gdl(gdl='chapter10.gdl', verbosity=None):
+    propnet = LogicNetwork()
+    parser.sentence.setParseAction(propnet.parse_action)
+    parser.game_description.setParseAction(noop)
+    for path in [gdl, os.path.join(os.path.dirname(__file__), '..', 'example_game_gdl', gdl)]:
+        try:
+            with open(path) as fpin:
+                gdl = fpin.read()
+            break
+        except:
+            pass
+    parser.game_description.parseString(gdl)
+    print propnet.nodes()
+    return propnet
+
+def propnet_from_example():
+    from gdl import parser
+    net = LogicNetwork()
+    parser.sentence.setParseAction(net.parse_action)
+    parser.game_description.parseFile('example_game_gdl/chapter10.gdl')
+    return net
 
 def test():
-    propnet = nx.MultiDiGraph()
+    net = nx.MultiDiGraph()
     # Example propnet from http://logic.stanford.edu/ggp/chapters/chapter_09.html
     s = Base('s')
     a = Input('a')
@@ -100,7 +208,7 @@ def test():
     # propnet.add_nodes_from((p, q, r))
     # propnet.add_nodes_from((asp, pq, rs))
 
-    propnet.add_edges_from([
+    net.add_edges_from([
                             (a, asp), (s, asp),
                             (asp, p),
                             (p, pq),
@@ -111,41 +219,16 @@ def test():
                             (rs, s),
                            ])
     from matplotlib import pyplot as plt
-    nx.draw(propnet, with_labels=True)
+    nx.draw(net, with_labels=True)
     plt.show()
-
+    parser.base.parseAction(Base())
     # game description from http://logic.stanford.edu/ggp/chapters/chapter_09.html
-    t = 
-    propnet.add_edges_from([
-                            (a, asp), (s, asp),
-                            (asp, p),
-                            (p, pq),
-                            (pq, q),
-                            (q, qbr),
-                            (b, qbr), (qbr, r),
-                            (r, rs),
-                            (rs, s),
-                           ])
-    gdl_example='''
-        (role(white))
-        (base(s))
-        (input(white a))
-        (input(white b))
-        (legal(white a))
-        (legal(white b))
-        (<= p does(white a) & true(s))
-        (<= q ~p)
-        (<= r true(q))
-        (<= r does(white b))
-        (<= next(s) r)
-        (<= terminal true(q))
-        (<= goal(white 100) true(s))
-        (<= goal(white 0) ~true(s))
-        '''
-    example = parser.game_description.parseString(gdl_example)
+
+    gdl_path = os.path.join(os.path.dirname(__file__), '..', 'example_game_gdl', 'chapter10.gdl')
+    example = parser.game_description.parseFile(gdl_path)
 
     propnet_computed =  network_from_parsed_gdl(example)
-    nx.draw(propnet, with_labels=True)
+    nx.draw(net, with_labels=True)
     plt.show()
-    return propnet
+    return net
 
